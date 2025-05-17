68ff088e3f4f3f85e0cd8a9983181871b89184d2
石李城 <985478238@qq.com>
2025-05-17 23:31:27 +0800

    Update QLApplication.kt

--- app/src/main/java/com/ql/ccs/QLApplication.kt
+++ app/src/main/java/com/ql/ccs/QLApplication.kt
@@ -7,7 +7,6 @@ import com.ql.ccs.dataModel.cabin.cabinType.coldCabin.EMB1
 import quickCanResolver.core.CanIo
 
 class QLApplication : Application() {
-    //lateinit var canIo : CanIo
     override fun onCreate() {
         super.onCreate()
         initCanIo()
@@ -17,8 +16,6 @@ class QLApplication : Application() {
         val canIo = CanIo.getInstance()
         // 2. 完成 数据模型的初始绑定
         canIo.manager.addDbcInputInterface{this.assets.open(CabinData.dbcPath1)}.bind(EMB1())
-
-
         Log.d(logTag,"-------------------------------  框架初始化完成  ------------------------------")
     }
     companion object{
